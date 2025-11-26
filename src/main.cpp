07283959d501029e5d8363c840892fc4b43acfd5
Lailaicat163 <lailaicat163@gmail.com>
2025-11-26 11:10:17 -0800

    Done commiting FINALLY

---
+++ src/main.cpp
@@ -0,0 +1,252 @@
+#pragma region VEXcode Generated Robot Configuration
+// Make sure all required headers are included.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+
+
+#include "vex.h"
+
+using namespace vex;
+
+// Brain should be defined by default
+brain Brain;
+
+
+// START V5 MACROS
+#define waitUntil(condition)                                                   \
+  do {                                                                         \
+    wait(5, msec);                                                             \
+  } while (!(condition))
+
+#define repeat(iterations)                                                     \
+  for (int iterator = 0; iterator < iterations; iterator++)
+// END V5 MACROS
+
+
+// Robot configuration code.
+motor leftMotorA = motor(PORT1, ratio18_1, false);
+motor leftMotorB = motor(PORT2, ratio18_1, false);
+motor leftMotorC = motor(PORT3, ratio18_1, false);
+motor_group LeftDriveSmart = motor_group(leftMotorA, leftMotorB, leftMotorC);
+motor rightMotorA = motor(PORT4, ratio18_1, true);
+motor rightMotorB = motor(PORT5, ratio18_1, true);
+motor rightMotorC = motor(PORT6, ratio18_1, true);
+motor_group RightDriveSmart = motor_group(rightMotorA, rightMotorB, rightMotorC);
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 299.24, 266.7, 190.5, mm, 1);
+
+controller Controller1 = controller(primary);
+
+motor IntakeMotor = motor(PORT12, ratio18_1, true);
+motor UpperMotor = motor(PORT13, ratio36_1, true);
+motor Second_IM = motor(PORT14, ratio18_1, true);
+
+digital_out Pneumatic_A = digital_out(Brain.ThreeWirePort.A);
+
+
+
+// controller Controller2 = controller(partner);
+
+// generating and setting random seed
+void initializeRandomSeed(){
+  int systemTime = Brain.Timer.systemHighResolution();
+  double batteryCurrent = Brain.Battery.current();
+  double batteryVoltage = Brain.Battery.voltage(voltageUnits::mV);
+
+  // Combine these values into a single integer
+  int seed = int(batteryVoltage + batteryCurrent * 100) + systemTime;
+
+  // Set the seed
+  srand(seed);
+}
+
+
+
+void vexcodeInit() {
+
+  //Initializing random seed.
+  initializeRandomSeed(); 
+}
+
+
+// Helper to make playing sounds from the V5 in VEXcode easier and
+// keeps the code cleaner by making it clear what is happening.
+void playVexcodeSound(const char *soundName) {
+  printf("VEXPlaySound:%s\n", soundName);
+  wait(5, msec);
+}
+
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+// define variables used for controlling motors based on controller inputs
+bool DrivetrainLNeedsToBeStopped_Controller1 = true;
+bool DrivetrainRNeedsToBeStopped_Controller1 = true;
+// define a task that will handle monitoring inputs from Controller1
+int rc_auto_loop_function_Controller1() {
+  // process the controller input every 20 milliseconds
+  // update the motors based on the input values
+  while(true) {
+    if(RemoteControlCodeEnabled) {
+      
+      // calculate the drivetrain motor velocities from the controller joystick axies
+      // left = Axis3 + Axis1
+      // right = Axis3 - Axis1
+      int drivetrainLeftSideSpeed = Controller1.Axis3.position() - Controller1.Axis1.position();
+      int drivetrainRightSideSpeed = Controller1.Axis3.position() + Controller1.Axis1.position();
+      
+      // check if the value is inside of the deadband range
+      if (drivetrainLeftSideSpeed < 5 && drivetrainLeftSideSpeed > -5) {
+        // check if the left motor has already been stopped
+        if (DrivetrainLNeedsToBeStopped_Controller1) {
+          // stop the left drive motor
+          LeftDriveSmart.stop();
+          // tell the code that the left motor has been stopped
+          DrivetrainLNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the left motor nexttime the input is in the deadband range
+        DrivetrainLNeedsToBeStopped_Controller1 = true;
+      }
+      // check if the value is inside of the deadband range
+      if (drivetrainRightSideSpeed < 5 && drivetrainRightSideSpeed > -5) {
+        // check if the right motor has already been stopped
+        if (DrivetrainRNeedsToBeStopped_Controller1) {
+          // stop the right drive motor
+          RightDriveSmart.stop();
+          // tell the code that the right motor has been stopped
+          DrivetrainRNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the right motor next time the input is in the deadband range
+        DrivetrainRNeedsToBeStopped_Controller1 = true;
+      }
+      
+      // only tell the left drive motor to spin if the values are not in the deadband range
+      if (DrivetrainLNeedsToBeStopped_Controller1) {
+        LeftDriveSmart.setVelocity(drivetrainLeftSideSpeed, percent);
+        LeftDriveSmart.spin(forward);
+      }
+      // only tell the right drive motor to spin if the values are not in the deadband range
+      if (DrivetrainRNeedsToBeStopped_Controller1) {
+        RightDriveSmart.setVelocity(drivetrainRightSideSpeed, percent);
+        RightDriveSmart.spin(forward);
+      }
+    }
+    // wait before repeating the process
+    wait(20, msec);
+  }
+  return 0;
+}
+
+task rc_auto_loop_task_Controller1(rc_auto_loop_function_Controller1);
+
+#pragma endregion VEXcode Generated Robot Configuration
+
+// ----------------------------------------------------------------------------
+//                                                                            
+//    Project:                                               
+//    Author:
+//    Created:
+//    Configuration:        
+//                                                                            
+// ----------------------------------------------------------------------------
+
+// Include the V5 Library
+#include "vex.h"
+#include <iostream>
+// Allows for easier use of the VEX Library
+using namespace vex;
+
+
+// Begin project code
+
+void preAutonomous(void) {
+  // actions to do when the program starts
+  Brain.Screen.clearScreen();
+  Brain.Screen.print("pre auton code");
+  wait(1, seconds);
+  Drivetrain.setDriveVelocity(100, percent);
+}
+
+void autonomous(void) {
+  Drivetrain.setDriveVelocity(100, percent);
+  Brain.Screen.clearScreen();
+  Brain.Screen.print("autonomous code");
+  IntakeMotor.setVelocity(100, percent);
+  UpperMotor.setVelocity(100, percent);
+  Second_IM.setVelocity(100, percent);
+  // place automonous code here
+  Drivetrain.driveFor(forward, 300, mm);
+  IntakeMotor.spin(forward);
+  Second_IM.spin(reverse);
+  wait(2, seconds);
+  IntakeMotor.stop();
+  Second_IM.stop();
+  Drivetrain.driveFor(reverse, 300, mm);
+}
+
+//following codes are for input purpose, if there are some bugs exist, 
+//then put the codes back to userc=Control
+void input(){
+  if(Controller1.ButtonR2.pressing() == true){
+    IntakeMotor.spin(forward);
+    Second_IM.spin(forward);
+  }
+  else if(Controller1.ButtonR1.pressing() == true){
+    IntakeMotor.spin(reverse);
+    Second_IM.spin(reverse);
+  }
+  else{
+    IntakeMotor.stop();
+    Second_IM.stop();
+  }
+  if(Controller1.ButtonL1.pressing() == true){
+    UpperMotor.spin(reverse);
+  }
+  else if(Controller1.ButtonL2.pressing() == true){
+    UpperMotor.spin(forward);
+  }
+  else{
+    UpperMotor.stop();
+  }
+}
+
+event Input;
+
+void userControl(void) {
+  LeftDriveSmart.setStopping(brake);
+  RightDriveSmart.setStopping(brake);
+  IntakeMotor.setStopping(brake);
+  IntakeMotor.setVelocity(100, percent);
+  // place driver control in this while loop
+  
+  while(true){
+    wait(10, msec);
+    input();
+  }  
+}
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  // create competition instance
+  competition Competition;
+  IntakeMotor.setVelocity(75, percent);
+  Second_IM.setVelocity(75, percent);
+  
+
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(userControl);
+  
+  
+  // Run the pre-autonomous function.
+  preAutonomous();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
